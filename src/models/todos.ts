5cd49f52e19853ebd49fca75e2f3593ea7b9d69c
Dipoengoro <dipoengoro@outlook.com>
2023-08-09 13:00:47 +0700

    Refine todos model

--- src/models/todos.ts
+++ src/models/todos.ts
@@ -1,41 +1,46 @@
 import Todo from './todo';
 
 class Todos {
-  private static _todos: Todo[] = [];
+  private readonly _todos: Todo[];
 
-  static get getAll(): Todo[] {
-    return Todos._todos;
+  constructor() {
+    this._todos = [];
   }
 
-  static findTodoById(todoId: string): Todo | undefined {
-    const todo = Todos._todos.find(todo => todo.id === todoId);
+  get getAll(): Todo[] {
+    return this._todos;
+  }
+
+  findTodoById(todoId: string): Todo | undefined {
+    const todo = this._todos.find(todo => todo.id === todoId);
     if (typeof todo === 'undefined') {
       return undefined;
     }
     return todo;
   }
 
-  static deleteTodoById(todoId: string): number {
-    const todoIndex = Todos._todos.findIndex(todo => todo.id === todoId);
+  deleteTodoById(todoId: string): number {
+    const todoIndex = this._todos.findIndex(todo => todo.id === todoId);
     if (todoIndex === -1) {
       return todoIndex;
     }
-    Todos._todos.splice(todoIndex, 1);
+    this._todos.splice(todoIndex, 1);
     return 0;
   }
 
-  static updateTodo(todoId: string, text: string): number {
-    const todo = Todos.findTodoById(todoId);
+  updateTodo(todoId: string, text: string): number {
+    const todo = this.findTodoById(todoId);
     if (typeof todo === 'undefined') {
       return -1;
     }
     todo.setText(text);
-    Todos._todos.push(todo);
+    this._todos.push(todo);
     return 0;
   }
 
-  static addTodo(todo: Todo): void {
-    Todos._todos.push(todo);
+  addTodo(todo: Todo): number {
+    this._todos.push(todo);
+    return 0;
   }
 }
 
